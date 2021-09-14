98c40e0693cc7d766d74e0c8a07c00a7f703aa65
biizheng <m15840149205@163.com>
2021-09-14 16:03:58 +0800

    初级内存管理系统完成(Chapter 8.5)

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,13 +1,23 @@
 #include "print.h"
 #include "init.h"
 #include "debug.h"
+#include "memory.h"
 int main(void)
 {
     put_char('\n');
     put_str("I am kernel\n");
+
+    put_str("memory size: ");
     put_int(*(uint32_t *)(0xb00));
+    put_str(" bytes");
     put_char('\n');
+
     init_all();
+
+    void *addr = get_kernel_pages(3);
+    put_str("\n get_kernel_page start vaddr is ");
+    put_int((uint32_t)addr);
+    put_str("\n");
     while (1)
         ;
     return 0;

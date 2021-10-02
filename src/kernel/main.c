f9f25a55a7626df928ab02056f96e56c907929f9
biizheng <m15840149205@163.com>
2021-10-02 08:30:31 +0800

    完善printf

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -17,17 +17,16 @@ int main(void)
 {
     put_str("I am kernel\n");
     init_all();
-    // clear_screen();
-    // set_cursor(0);
-    process_execute(u_prog_a, "user_prog_a");
-    process_execute(u_prog_b, "user_prog_b");
 
-    intr_enable();
-    console_put_str(" main_pid:0x");
+    process_execute(u_prog_a, "u_prog_a");
+    process_execute(u_prog_b, "u_prog_b");
+
+    console_put_str(" I am main, my pid:0x");
     console_put_int(sys_getpid());
     console_put_char('\n');
-    thread_start("k_thread_a", 31, k_thread_a, "argA ");
-    thread_start("k_thread_b", 31, k_thread_b, "argB ");
+    intr_enable();
+    thread_start("k_thread_a", 31, k_thread_a, "I am thread_a");
+    thread_start("k_thread_b", 31, k_thread_b, "I am thread_b ");
     while (1)
         ;
     return 0;
@@ -37,7 +36,7 @@ int main(void)
 void k_thread_a(void *arg)
 {
     char *para = arg;
-    console_put_str(" thread_a_pid:0x");
+    console_put_str(" I am thread_a, my pid:0x");
     console_put_int(sys_getpid());
     console_put_char('\n');
     while (1)
@@ -48,7 +47,7 @@ void k_thread_a(void *arg)
 void k_thread_b(void *arg)
 {
     char *para = arg;
-    console_put_str(" thread_b_pid:0x");
+    console_put_str(" I am thread_b, my pid:0x");
     console_put_int(sys_getpid());
     console_put_char('\n');
     while (1)
@@ -58,15 +57,17 @@ void k_thread_b(void *arg)
 /* 测试用户进程 */
 void u_prog_a(void)
 {
-    printf(" prog_a_pid:0x%x\n", getpid());
-    while (1){}
+    char *name = "prog_a";
+    printf(" I am %s, my pid:%d%c", name, getpid(), '\n');
+    while (1)
         ;
 }
 
 /* 测试用户进程 */
 void u_prog_b(void)
 {
-    printf(" prog_b_pid:0x%x\n", getpid());
+    char *name = "prog_b";
+    printf(" I am %s, my pid:%d%c", name, getpid(), '\n');
     while (1)
         ;
 }
